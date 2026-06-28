f49b3ae545e3115862aa8a112e2537d9bf3aac4c
Gabriel Castro <ionebielcastro@gmail.com>
2026-06-28 13:09:50 -0300

    feat:  add GET /transactions endpoint

--- backend-java/src/main/java/com/financer/IA/controller/TransactionController.java
+++ backend-java/src/main/java/com/financer/IA/controller/TransactionController.java
@@ -6,10 +6,9 @@ import com.financer.IA.service.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/transactions")
@@ -22,5 +21,11 @@ public class TransactionController {
     public ResponseEntity<Transaction> createTransaction(@RequestBody TransactionDTO transactionDTO) {
         Transaction transaction = transactionService.save(transactionDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(transaction);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Transaction>> getAllTransactions() {
+        List<Transaction> transactions = transactionService.findAll();
+        return ResponseEntity.ok(transactions);
     }
 }
